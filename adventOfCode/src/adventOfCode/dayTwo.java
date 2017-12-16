3d1404a1af94c080227bfbae726d1c928c9590c0
zenko44 <zenko44@gmail.com>
2017-12-16 22:55:41 +0000

    Day one complete
    Day one completed, day two failing on out of bounds exception -1

--- adventOfCode/src/adventOfCode/dayTwo.java
+++ adventOfCode/src/adventOfCode/dayTwo.java
@@ -3,38 +3,72 @@ package adventOfCode;
 import java.util.ArrayList;
 
 public class dayTwo {
-	
-	public String input = "day2.txt";
-	
+
+	public String input = inputLoader.getInput("day2.txt");
+
 	public static void main(String[] args) {
-			dayTwo day = new dayTwo();
-			
-			System.out.println("part 1" + day.partOne(day.input));
-			
-			//System.out.println("part 2 "+ day.partTwo(day.input));
-			
+		dayTwo day = new dayTwo();
+
+		System.out.println("part 1" + day.partOne(day.input));
+
+		System.out.println("part 2 " + day.partTwo(day.input));
+
 	}
-	
+
 	public String partOne(String input) {
-		ArrayList<Integer> checksums = new ArrayList();
-		for(String line : input.split("\n")) {
-			line = line.trim().replaceAll("+", " ");
+		ArrayList<Integer> checksums = new ArrayList<>();
+		for (String line : input.split("\n")) {
+			line = line.trim().replaceAll(" +", " ");
 			ArrayList<Integer> nums = new ArrayList<>();
-			while(!line.trim().isEmpty()) {
+			while (!line.trim().isEmpty()) {
 				nums.add(Integer.parseInt(line.substring(0, line.indexOf(" "))));
 				line = line.substring(line.indexOf(" ")).trim() + " ";
 			}
+
 			int highest = nums.get(0), lowest = nums.get(0);
-			for(int num:nums) {
-				if(num > highest) highest = num;
-				else if (num < lowest) lowest = num;
+			for (int num : nums) {
+				if (num > highest)
+					highest = num;
+				else if (num < lowest)
+					lowest = num;
+
 			}
 			checksums.add(highest - lowest);
 		}
+
 		int total = 0;
-		for (int checksum : checksums) {
+		for (int checksum : checksums)
 			total += checksum;
-		}return String.valueOf(total);
+		return String.valueOf(total);
+	}
+
+	public String partTwo(String input) {
+		int total = 0;
+		for (String line : input.split("\n")) {
+			line = line.trim().replaceAll(" +", " ");
+			ArrayList<Integer> nums = new ArrayList<>();
+			while (!line.trim().isEmpty()) {
+				nums.add(Integer.parseInt(line.substring(0, line.indexOf(" "))));
+				line = line.substring(line.indexOf(" ")).trim() + " ";
+			}
+
+			for (int i = 0; i < nums.size() - 1; i++) {
+				for (int j = i + 1; j < nums.size(); j++) {
+					int a = nums.get(i), b = nums.get(j);
+					if (a > b) {
+						if (a % b == 0) {
+							total += a / b;
+						}
+					} else if (b > a) {
+						if (b % a == 0) {
+							total += b / a;
+						}
+					}
+				}
+			}
+		}
+
+		return String.valueOf(total);
 	}
 
 }
--- adventOfCode/src/adventOfCode/inputLoader.java
+++ adventOfCode/src/adventOfCode/inputLoader.java
@@ -5,22 +5,25 @@ import java.io.File;
 import java.io.FileReader;
 
 public class inputLoader {
-	
+
 	public static String getInput(String filename) {
 		try {
-			File f = new File("src" + File.separator + "adventOfCode" + File.separator + "resources" + File.separator + filename);
+			File f = new File("src" + File.separator + "adventOfCode" + File.separator + filename);
 			BufferedReader br = new BufferedReader(new FileReader(f));
+
 			StringBuilder data = new StringBuilder();
 			String line;
-			
 			while ((line = br.readLine()) != null)
 				data.append(line).append('\n');
-			
+			br.close();
+
 			return data.toString();
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			return"";
+			return "";
 		}
+
 	}
-	
+
 }
